21b23048076e1c9e7a92bc41d6612454c9d8b75e
Click <70248161+click675@users.noreply.github.com>
2023-02-16 17:33:45 +0700

    added new files

---
+++ TOH_Disk.py
@@ -0,0 +1,14 @@
+class Disk(object):
+    def __init__(self,name="",xpos=0,ypos=0,height=20,width=40):
+        self.dname = name
+        self.dxpos = xpos
+        self.dypos = ypos
+        self.dheight = height
+        self.dwidth = width
+
+        
+    def showdisk(self):
+        
+    def newpos(self,xpos,ypos):
+    
+    def cleardisk(self):
---
+++ TOH_Pole.py
@@ -0,0 +1,15 @@
+class Pole(object):
+    def __init__(self,name="",xpos=0,ypos=0,thick=10,length=100):
+        self.pname = name
+        self.stack = []
+        self.toppos = 0
+        self.pxpos = xpos
+        self.pypos = ypos
+        self.pthick = thick
+        self.plength = length
+        
+    def showpole(self):
+        
+    def pushdisk(self,disk):
+    
+    def popdisk(self):
