8262904c42361244cdef94c5f989448e624c1d53
enlighteningCrow <87510657+enlighteningCrow@users.noreply.github.com>
2023-02-16 18:17:34 +0700

    Fixed some parts of the Hanoi

--- TOH_Pole.py
+++ TOH_Pole.py
@@ -1,15 +1,16 @@
 import turtle
 
+
 class Pole(object):
-    def __init__(self,name="",xpos=0,ypos=0,thick=10,length=100):
+
+    def __init__(self, name = "", xpos = 0, ypos = 0, thick = 10, length = 100):
         self.pname = name
         self.stack = []
-        self.toppos = 0
         self.pxpos = xpos
         self.pypos = ypos
         self.pthick = thick
         self.plength = length
-        
+
     def showpole(self):
         self.drawpole = turtle.Turtle()
         self.drawpole.speed(0)
@@ -21,14 +22,18 @@ class Pole(object):
         self.drawpole.forward(self.plength)
         self.drawpole.penup()
         self.drawpole.goto(self.pxpos, self.pypos + 20)
-        self.drawpole.write(self.pname, align="center", font=("Arial", 16, "bold"))
+        self.drawpole.write(
+            self.pname, align = "center", font = ("Arial", 16, "bold")
+        )
         self.drawpole.goto(self.pxpos, self.pypos)
-        
-    def pushdisk(self,disk):
+
+    def pushdisk(self, disk):
+        print(self.stack)
         self.stack.append(disk)
-        self.toppos = self.toppos + 1
-        disk.setpos(self.pxpos, self.pypos + self.toppos * 20)
+        disk.newpos(self.pxpos, self.pypos + len(self.stack) * 20)
         disk.showdisk()
-        
+        print(self.stack)
+
     def popdisk(self):
-        
+        print(self.stack)
+        return self.stack.pop()
