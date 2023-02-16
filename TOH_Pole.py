e5dfad6f63e5ced977bffc8c1b9804dbde487c18
Click <70248161+click675@users.noreply.github.com>
2023-02-16 17:48:55 +0700

    added some functions in TOH_Pole

--- TOH_Pole.py
+++ TOH_Pole.py
@@ -1,3 +1,5 @@
+import turtle
+
 class Pole(object):
     def __init__(self,name="",xpos=0,ypos=0,thick=10,length=100):
         self.pname = name
@@ -9,7 +11,24 @@ class Pole(object):
         self.plength = length
         
     def showpole(self):
+        self.drawpole = turtle.Turtle()
+        self.drawpole.speed(0)
+        self.drawpole.hideturtle()
+        self.drawpole.penup()
+        self.drawpole.goto(self.pxpos, self.pypos)
+        self.drawpole.pendown()
+        self.drawpole.pensize(self.pthick)
+        self.drawpole.forward(self.plength)
+        self.drawpole.penup()
+        self.drawpole.goto(self.pxpos, self.pypos + 20)
+        self.drawpole.write(self.pname, align="center", font=("Arial", 16, "bold"))
+        self.drawpole.goto(self.pxpos, self.pypos)
         
     def pushdisk(self,disk):
-    
+        self.stack.append(disk)
+        self.toppos = self.toppos + 1
+        disk.setpos(self.pxpos, self.pypos + self.toppos * 20)
+        disk.showdisk()
+        
     def popdisk(self):
+        
