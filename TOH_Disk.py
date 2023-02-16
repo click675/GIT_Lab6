9a84ba9ef90bc5d33d0682926376f3aa2f583586
Click <70248161+click675@users.noreply.github.com>
2023-02-16 17:52:37 +0700

    add draw in TOH_Disk

--- TOH_Disk.py
+++ TOH_Disk.py
@@ -1,3 +1,5 @@
+import turtle
+
 class Disk(object):
     def __init__(self,name="",xpos=0,ypos=0,height=20,width=40):
         self.dname = name
@@ -8,6 +10,19 @@ class Disk(object):
 
         
     def showdisk(self):
+        self.drawdisk = turtle.Turtle()
+        self.drawdisk.speed(0)
+        self.drawdisk.hideturtle()
+        self.drawdisk.penup()
+        self.drawdisk.goto(self.dxpos, self.dypos)
+        self.drawdisk.pendown()
+        self.drawdisk.begin_fill()
+        for i in range(2):
+            self.drawdisk.forward(self.dwidth)
+            self.drawdisk.left(90)
+            self.drawdisk.forward(self.dheight)
+            self.drawdisk.left(90)
+        self.drawdisk.end_fill()
         
     def newpos(self,xpos,ypos):
     
