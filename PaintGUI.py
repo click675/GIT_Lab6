b3e6d0a721061c5e3555706e9df4be7f7c9207cf
Click <70248161+click675@users.noreply.github.com>
2023-02-16 17:22:49 +0700

    added paint

--- PaintGUI.py
+++ PaintGUI.py
@@ -2,3 +2,8 @@ import sys
 from PySide6.QtCore import *
 from PySide6.QtWidgets import *
 from PySide6.QtGui import *
+
+class Paint(QWidget):
+     def __init__(self):
+        QWidget.__init__(self, None)
+        self.setWindowTitle("A simple paint program")
